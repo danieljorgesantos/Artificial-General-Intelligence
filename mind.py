a7d7bcf4e956688e64aea2cec768031a5e8fca21
Daniel Santos <daniel.jorge.telo.filipesantoss@gmail.com>
2023-06-17 18:59:15 +0100

    added

--- mind.py
+++ mind.py
@@ -1,9 +1,28 @@
+class inputSensoryInformation:
+    def __init__(self):
+        pass
+
+    def visionPhotoreceptors(self, visionInput):
+        pass
+
+    def hearingVibrationCells(self, hearingInput):
+        pass
+
+    def abstractInputForTest(self, abstractInput):
+        pass
+
 class MindGraph:
     def __init__(self):
         self.mind = {}
         self.state = {}
 
-    def add_cell(self, u, v):
+    # initialize propagation in the mind from inputSensoryInformation changing the state of each cell
+    def thalamus(self, entrySignal):
+        pass
+
+    # internal management process
+    # Creation of new cells
+    def neurogenesis(self, u, v):
         if u in self.mind:
             self.mind[u].append(v)
         else:
@@ -14,25 +33,33 @@ class MindGraph:
         else:
             self.mind[v] = [u]
 
+    # internal management process
     def get_neighbors(self, u):
         if u in self.mind:
             return self.mind[u]
         else:
             return []
-        
+
+    # Synaptic plasticity refers to the ability of synapses to change their strength or form new connections.
+    # This process is crucial for learning and memory. When two neurons repeatedly activate together,
+    # the synapse between them becomes strengthened, making future communication more efficient.
+    def synapticPlasticity(self):
+        pass
+
+    # checks the state of the cells
     def snapshot(self):
         pass
 
 # Create an instance
 mind = MindGraph()
 
-# Add edges to the graph
-mind.add_cell(1, 2)
-mind.add_cell(2, 3)
-mind.add_cell(3, 4)
-mind.add_cell(4, 1)
+# Add axon (edge) to other cell
+mind.neurogenesis(1, 2)
+mind.neurogenesis(2, 3)
+mind.neurogenesis(3, 4)
+mind.neurogenesis(4, 1)
 
-# Each one of these has its own will.
+# Each one of these has its own mechanism.
 
 # Get the neighbors of a vertex
 neighbors = mind.get_neighbors(1)
@@ -44,5 +71,7 @@ state={
     3:0,
     4:0
 }
+
+
 
 
